6926fb49de503917e23246544eccdf789bc706d1
Nazmus Ashrafi <nazmus.s.ashrafi@gmail.com>
2022-10-17 16:00:50 +0600

    initial commit

---
+++ Models/userModel.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true
+        },
+        password : {
+            type: String,
+            required: true
+        },
+        email : {
+            type: String,
+            required: false
+        },
+        firstname: {
+            type: String,
+            required: true
+        },
+        lastname : {
+            type: String,
+            required: true
+        },
+        shopname : {
+            type: String,
+            required: false
+        },
+        isAdmin : {
+            type: Boolean,
+            default: false,
+        },
+        profileImage : {
+            type: Array
+            
+        },
+        coverPicture: String,
+        about: String,
+        livesin: String,
+        worksAt: String,
+        relationship: String,
+        followers: [] ,
+        following: [] ,
+        notifications: {
+            type: Array
+        },
+        description: String,
+        resetToken: {
+            type: String,
+            default: ''
+        },
+        expireToken: {
+            type: Date,
+        },
+        isShop: {
+            type: Boolean,
+            default: false
+        }
+
+    },
+    {timestamps: true}
+)
+
+const UserModel = mongoose.model("Users", UserSchema);
+export default UserModel
